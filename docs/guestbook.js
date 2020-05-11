d0ab09616f01afd6c6cbd8a4c9f29716fa22f8f9
Haoran Mo <m.haoran.cq@gmail.com>
2020-05-11 17:15:04 +0400

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://f8cd0a15.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
